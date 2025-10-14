c11d64928e6bb2bfbb8953f351d4e83b581ead4d
Cláudia Martins <dev.acc.4.claudia.martins@gmail.com>
2025-10-14 18:43:43 +0100

    added rules to eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -3,5 +3,17 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    rules: {
+      "no-var": "error", // Disallow the use of 'var'
+      semi: ["error", "always"], // Require semicolons at the end of statements
+      "no-console": "warn", // Warn if 'console.log' is left in the code
+    },
+  },
 ]);
